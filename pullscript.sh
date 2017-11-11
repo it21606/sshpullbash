590a132a9f45fc338e9db78e07a82e87b32761c3
Konstantinos Vaismenos <it21606@hua.gr>
2017-11-11 17:41:10 +0200

    Update pullscript.sh

--- pullscript.sh
+++ pullscript.sh
@@ -5,10 +5,10 @@ NC=`tput sgr0`
 echo "Please provide your it username${RED}(ex.it21606)${NC}:"
 read it
 
-ssh -tt $it@10.100.51.113 << HERE
-cp home/tsadimas/it_users.txt ~/Desktop
-~.
-HERE
+ssh -tt $it@10.100.51.113 << ENDSSH
+cp /home/tsadimas/it_users.txt ~/Desktop && exit
+ENDSSH
+
 
 echo "${RED}Copy from instructor's directory successful${NC}"
 echo "Please re-enter your password to download file to local machine"
@@ -19,7 +19,7 @@ read ans
 if [ "$ans" == "y" ] 
 then
 echo "Please re-enter your password to delete file from remote machine"
-ssh $it@10.100.51.113 << HERE
+ssh -tt $it@10.100.51.113 << HERE
 rm ~/Desktop/it_users.txt'
 ~.
 HERE
