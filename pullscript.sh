0dc225e101cf4932623a7f6f5f8c76dbfa51d2b6
Konstantinos Vaismenos <it21606@hua.gr>
2017-11-11 14:58:03 +0200

    Update pullscript.sh

--- pullscript.sh
+++ pullscript.sh
@@ -13,7 +13,7 @@ scp $it@10.100.51.113:~/Desktop/it_users.txt ~/Desktop.
 echo "Copy to local machine successful"
 echo "Do you to delete remote file;(y/n)"
 read ans
-if [ $ans == "y" ] 
+if [ "$ans" == "y" ] 
 then
 echo "Please re-enter your password to delete file from remote machine"
 ssh $it@10.100.51.113 'rm ~/Desktop/it_users.txt'
