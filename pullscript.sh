2cd59d69e1fe1f8962b7c1ddf4f36a3612dc734a
Konstantinos Vaismenos <it21606@hua.gr>
2017-11-11 17:17:00 +0200

    Update pullscript.sh

--- pullscript.sh
+++ pullscript.sh
@@ -1,21 +1,27 @@
 #! /bin/bash
-
-echo "Please provide your it username(ex.it21606)"
+clear
+RED=`tput setaf 1`
+NC=`tput sgr0`
+echo "Please provide your it username${RED}(ex.it21606)${NC}:"
 read it
 
-ssh $it@10.100.51.113 << HERE
-cp ~/tsadimas/it_users.txt ~/Desktop
+ssh -tt $it@10.100.51.113 << HERE
+cp home/tsadimas/it_users.txt ~/Desktop
 ~.
 HERE
-echo "Copy from instructor's directory successful"
+
+echo "${RED}Copy from instructor's directory successful${NC}"
 echo "Please re-enter your password to download file to local machine"
-scp $it@10.100.51.113:~/Desktop/it_users.txt ~/Desktop.
+scp $it@10.100.51.113:~/Desktop/it_users.txt ~/Desktop
 echo "Copy to local machine successful"
-echo "Do you to delete remote file;(y/n)"
+echo "Do you to delete remote file;(y/${RED}n${NC})"
 read ans
 if [ "$ans" == "y" ] 
 then
 echo "Please re-enter your password to delete file from remote machine"
-ssh $it@10.100.51.113 'rm ~/Desktop/it_users.txt'
+ssh $it@10.100.51.113 << HERE
+rm ~/Desktop/it_users.txt'
 ~.
+HERE
 fi
+exit
